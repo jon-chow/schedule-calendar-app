309796593fc9dc803204a52b141790fcaeedb568
Jon Chow <70170720+jon-chow@users.noreply.github.com>
2022-09-22 13:26:17 -0700

    added calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,13 @@ import parse from 'date-fns/parse';
 import startOfWeek from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
 
+/* Classes */
+import { Events } from './classes/Events';
+
 /* Styles */
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './App.css';
+import './App.scss';
 
 
 const locales = {
@@ -28,6 +31,12 @@ const localizer = dateFnsLocalizer({
   locales,
 })
 
+const events = [
+  new Events('Event 1', new Date(2023,0,3), new Date(2023,0,4)),
+  new Events('Event 2', new Date(2023,0,6), new Date(2023,0,12)),
+  new Events('Event 3', new Date(2023,0,7), new Date(2023,0,15)),
+]
+
 /**
  * Returns the react app component.
  * @returns JSX.Element
@@ -35,7 +44,20 @@ const localizer = dateFnsLocalizer({
 const App = () => {
   return (
     <div className="App">
-      
+      <Calendar
+        className="Calendar"
+        localizer={localizer}
+        events={events.map((event) => {
+          return {
+            title: event.Title,
+            allDay: event.IsAllDay,
+            start: event.StartDate,
+            end: event.EndDate,
+          }
+        })}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   );
 }
